c8a49c42dfc6a5df6efcb8df756ba446d216834a
Girish11k <68158466+Girish11k@users.noreply.github.com>
2021-05-20 16:50:35 +0530

    Commit UC3 push

---
+++ src/com/Snake/Main.java
@@ -0,0 +1,21 @@
+package com.Snake;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to SnakeAndLadder Game");
+
+        int position=0;
+        System.out.println("Player start position : "+position);
+        Random random = new Random();
+
+        int dice = 0;
+        while (true)
+        {
+            dice = random.nextInt(7);
+            if(dice !=0) break;
+        }
+        System.out.println(dice);
+    }
+}
